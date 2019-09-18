340f505d559a943378cf67bcb3ba08bcf10b0ede
snr1988 <snr1988@gmail.com>
2019-09-18 08:23:17 +0530

    add new

---
+++ src/main/java/war/project.war/Application.java
@@ -0,0 +1,15 @@
+package project009.com.project.war;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("project009.com.project.war.mapper")
+public class Application {
+
+	public static void main(String[] args)  {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
